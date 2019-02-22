3edae9355863a834a79ff73eefa9c2f5be621f28
Skyteck <sky.armada@gmail.com>
2019-02-22 14:42:21 -0600

    first refactor and basics are set up

---
+++ ConsoleApp2/LCRClient.cs
@@ -0,0 +1,67 @@
+﻿using LiteNetLib;
+using LiteNetLib.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCR
+{
+    class LCRClient : INetEventListener
+    {
+        public NetManager client;
+        private readonly NetPacketProcessor _netPacketProcessor = new NetPacketProcessor();
+
+        public LCRClient()
+        {
+            _netPacketProcessor.RegisterNestedType(() => new pl());
+            _netPacketProcessor.SubscribeReusable<pl, NetPeer>(ProcessPacket);
+        }
+
+        public void OnConnectionRequest(ConnectionRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNetworkError(IPEndPoint endPoint, SocketError socketError)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNetworkLatencyUpdate(NetPeer peer, int latency)
+        {
+
+        }
+
+        public void OnNetworkReceive(NetPeer peer, NetPacketReader reader, DeliveryMethod deliveryMethod)
+        {
+            Console.WriteLine($"[Client {DateTime.Now}] Received data. Processing...");
+            //_netPacketProcessor.ReadAllPackets(dataReader, fromPeer); // LiteNetLib.Utils.ParseException: 'Undefined packet in NetDataReader'
+        }
+
+        public void OnNetworkReceiveUnconnected(IPEndPoint remoteEndPoint, NetPacketReader reader, UnconnectedMessageType messageType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnPeerConnected(NetPeer peer)
+        {
+            Console.WriteLine($"[Client] Connected to {peer.EndPoint}");
+        }
+
+        public void OnPeerDisconnected(NetPeer peer, DisconnectInfo disconnectInfo)
+        {
+            Console.WriteLine($"[Client] Connection to {peer.EndPoint} died cause {disconnectInfo.Reason}");
+            //die = true;
+        }
+
+        public void ProcessPacket(pl thePl, NetPeer peer)
+        {
+
+        }
+    }
+
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,101 +4,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 
-namespace ConsoleApp2
+namespace LCR
 {
     class Program
     {
         static void Main(string[] args)
         {
             NetPacketProcessor _netPacketProcessor = new NetPacketProcessor();
-            EventBasedNetListener listener = new EventBasedNetListener();
+            LCRClient listener = new LCRClient();
             NetManager client = new NetManager(listener);
+            listener.client = client;
             client.Start();
-            bool die = false;
-            var t = client.Connect("localhost" /* host ip or name */, 9050 /* port */, "SomeConnectionKey" /* text key or NetDataWriter */);
+                       
+            var server = client.Connect("localhost" /* host ip or name */, 9050 /* port */, "SomeConnectionKey" /* text key or NetDataWriter */);
 
-            listener.PeerConnectedEvent += peer => 
-            {
-                Console.WriteLine($"Connected to {peer.EndPoint}");
-            };
-
-            listener.PeerDisconnectedEvent += (peer, info) =>
-            {
-                Console.WriteLine($"Connection to {peer.EndPoint} died cause {info.Reason}");
-                die = true;
-            };
-
-            listener.NetworkReceiveEvent += (fromPeer, dataReader, deliveryMethod) =>
-            {
-                Console.WriteLine("We got: {0}", dataReader.GetString(100 /* max length of string */));
-                dataReader.Recycle();
-            };
-            pl mpl = new pl();
-            _netPacketProcessor.RegisterNestedType<pl>(() => new pl());
-            //_netPacketProcessor.SubscribeReusable<SamplePacket, NetPeer>(Ontest);
+            pl mpl = new pl { Name = "Bob", Tickets = 3};
+            NetDataWriter dw = new NetDataWriter();
 
             SamplePacket samPack = new SamplePacket { testPl = mpl };
 
-            _netPacketProcessor.SendNetSerializable<SamplePacket>(client, samPack, DeliveryMethod.ReliableOrdered);
-            //_netPacketProcessor.Write<pl>(dw, mpl);
-            //_netPacketProcessor.Send(t, samPack, DeliveryMethod.ReliableOrdered);
-            //client.SendToAll(_netPacketProcessor.Write(samPack), DeliveryMethod.ReliableOrdered);
-            //client.SendToAll(dw, DeliveryMethod.ReliableOrdered);
-            while (!Console.KeyAvailable)
+            do
             {
                 client.PollEvents();
                 Thread.Sleep(15);
-                if (die)
-                    return;
-            }
+
+                if (server.ConnectionState == ConnectionState.Connected)
+                {
+                    string input = Console.ReadLine();
+                    if (input == "r")
+                    {
+                        _netPacketProcessor.Send<pl>(server, mpl, DeliveryMethod.ReliableOrdered);
+                    }
+                }
+            } while (server.ConnectionState == ConnectionState.Connected);
 
             client.Stop();
-        }
-
-        public void Ontest(SamplePacket sp, NetPeer p)
-        {
-            Console.WriteLine("[Server] Received Packet!");
-        }
-    }
-
-    class SamplePacket : INetSerializable
-    {
-        public pl testPl { get; set; }
-
-        public void Deserialize(NetDataReader reader)
-        {
-            testPl.Deserialize(reader);
-        }
-
-        public void Serialize(NetDataWriter writer)
-        {
-            testPl.Serialize(writer);
-        }
-    }
-
-    public class pl : INetSerializable
-    {
-        public string Name { get; set; }
-        public int Tickets { get; set; }
-        public delegate pl reee();
-        public pl()
-        {
-        }
-        public void Deserialize(NetDataReader reader)
-        {
-            Name = reader.GetString();
-            Tickets = reader.GetInt();
-        }
-
-        public void Serialize(NetDataWriter writer)
-        {
-            writer.Put(Tickets);
-            writer.Put(Name);
         }
     }
 }
