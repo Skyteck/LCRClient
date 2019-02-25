6874901bc24b65429c9fe7a313d4a1e395dde99a
Skyteck <sky.armada@gmail.com>
2019-02-25 16:13:56 -0600

    Lobby creation

--- ConsoleApp2/LCRClient.cs
+++ ConsoleApp2/LCRClient.cs
@@ -14,12 +14,42 @@ namespace LCR
     class LCRClient : INetEventListener
     {
         public NetManager client;
+        private NetPeer Server;
         private readonly NetPacketProcessor _netPacketProcessor = new NetPacketProcessor();
+        public LCRLobby theGame;
+        public pl tpl;
+        private string ClientSignature
+        {
+            get
+            {
+                return $"[Client {DateTime.Now}]";
+            }
+        }
 
         public LCRClient()
         {
             _netPacketProcessor.RegisterNestedType(() => new pl());
             _netPacketProcessor.SubscribeReusable<pl, NetPeer>(ProcessPacket);
+            _netPacketProcessor.SubscribeReusable<CreateGamePacket, NetPeer>(ProcessNewGame);
+        }
+
+        private void ProcessNewGame(CreateGamePacket arg1, NetPeer arg2)
+        {
+            if(arg1.Success)
+            {
+                Console.WriteLine($"{ClientSignature} game created! ID:{arg1.Id} Players:{arg1.Playernum}");
+                theGame = new LCRLobby(arg1.Id, arg1.Playernum);
+            }
+            else
+            {
+                Console.WriteLine($"{ClientSignature} game failed! ID:{arg1.Id} Players:{arg1.Playernum}");
+
+            }
+        }
+
+        public void AttachServer(NetPeer p)
+        {
+            Server = p;
         }
 
         public void OnConnectionRequest(ConnectionRequest request)
@@ -39,8 +69,14 @@ namespace LCR
 
         public void OnNetworkReceive(NetPeer peer, NetPacketReader reader, DeliveryMethod deliveryMethod)
         {
-            Console.WriteLine($"[Client {DateTime.Now}] Received data. Processing...");
-            _netPacketProcessor.ReadAllPackets(reader, peer); // LiteNetLib.Utils.ParseException: 'Undefined packet in NetDataReader'
+            Console.WriteLine($"{ClientSignature} Received data. Processing...");
+            _netPacketProcessor.ReadAllPackets(reader, peer);
+        }
+
+        public void ProcessPacket(pl thePl, NetPeer peer)
+        {
+            Console.WriteLine($"{thePl.Name} {thePl.Tickets}");
+            tpl = thePl;
         }
 
         public void OnNetworkReceiveUnconnected(IPEndPoint remoteEndPoint, NetPacketReader reader, UnconnectedMessageType messageType)
@@ -50,18 +86,23 @@ namespace LCR
 
         public void OnPeerConnected(NetPeer peer)
         {
-            Console.WriteLine($"[Client] Connected to {peer.EndPoint}");
+            Console.WriteLine($"{ClientSignature} Connected to {peer.EndPoint}");
         }
 
         public void OnPeerDisconnected(NetPeer peer, DisconnectInfo disconnectInfo)
         {
-            Console.WriteLine($"[Client] Connection to {peer.EndPoint} died cause {disconnectInfo.Reason}");
-            //die = true;
+            Console.WriteLine($"{ClientSignature} Connection to {peer.EndPoint} died cause {disconnectInfo.Reason}");
         }
 
-        public void ProcessPacket(pl thePl, NetPeer peer)
+        public void SendPacket(pl thePl)
         {
-            Console.WriteLine($"{thePl.Name} {thePl.Tickets}");
+            _netPacketProcessor.Send<pl>(Server, thePl, DeliveryMethod.ReliableOrdered);
+        }
+
+        public void SendCreateGameRequest(CreateGamePacket cgp)
+        {
+            Console.WriteLine($"{ClientSignature} creating game: ID:{cgp.Id}  Players:{cgp.Playernum}");
+            _netPacketProcessor.SendNetSerializable<CreateGamePacket>(Server, cgp, DeliveryMethod.ReliableOrdered);
         }
     }
 
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -24,27 +24,47 @@ namespace LCR
             client.Start();
                        
             var server = client.Connect("localhost" /* host ip or name */, 9050 /* port */, "SomeConnectionKey" /* text key or NetDataWriter */);
-
+            client.DisconnectTimeout = 6000;
             pl mpl = new pl { Name = "Bob", Tickets = 3};
             NetDataWriter dw = new NetDataWriter();
-
+            listener.tpl = mpl;
             SamplePacket samPack = new SamplePacket {  };
 
-            Thread.Sleep(100);
-            do
+
+            Thread.Sleep(500);
+            while (server.ConnectionState != ConnectionState.Connected)
+            {
+                int cd = 1000;
+                Console.WriteLine($"[Client {DateTime.Now}] connection to localhost port 9050 failed. Trying again in {cd} ms.");
+
+                Thread.Sleep(cd);
+                server = client.Connect("localhost" /* host ip or name */, 9050 /* port */, "SomeConnectionKey" /* text key or NetDataWriter */);
+            }
+            listener.AttachServer(server);
+            while (server.ConnectionState == ConnectionState.Connected)
             {
                 client.PollEvents();
                 Thread.Sleep(15);
 
-                if (server.ConnectionState == ConnectionState.Connected)
+                if (Console.KeyAvailable)
                 {
-                    string input = Console.ReadLine();
-                    if (input == "r")
+                    var key = Console.ReadKey(false);
+                    if(key.KeyChar == 'r')
                     {
-                        _netPacketProcessor.Send<pl>(server, mpl, DeliveryMethod.ReliableOrdered);
+                        listener.SendPacket(listener.tpl);
+                    }
+                    else if(key.KeyChar == 'c')
+                    {
+                        CreateGamePacket cgp = new CreateGamePacket();
+                        listener.SendCreateGameRequest(cgp);
                     }
                 }
-            } while (server.ConnectionState == ConnectionState.Connected);
+
+                if (server.ConnectionState != ConnectionState.Connected)
+                {
+                    break;
+                }
+            } 
 
             client.Stop();
         }
